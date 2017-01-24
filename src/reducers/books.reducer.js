9894f54c5ca88e8b85fc4ae191736ec804c20997
Dan Hinze <hinzed1127@gmail.com>
2017-01-23 23:49:43 -0600

    WIP wiring in the books reducer

---
+++ src/reducers/books.reducer.js
@@ -0,0 +1,24 @@
+export default function() {
+  return [
+    {
+      title: 'Swing Time',
+      author: 'Zadie Smith'
+    },
+    {
+      title: 'Evicted'
+      author: 'Matthew Desmond'
+    },
+    {
+      title: 'On Beauty',
+      author: 'Zadie Smith'
+    },
+    {
+      title: 'A Heartbreaking Work of Staggering Genius',
+      author: 'Dave Eggers'
+    },
+    {
+      title: 'Between the World and Me',
+      author: 'Ta-Nehisi Coates'
+    }
+  ];
+}
